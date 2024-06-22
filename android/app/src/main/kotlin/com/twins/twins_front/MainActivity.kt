990b6f23c93c8afcff2d04d239e960ad53cc29c7
DenisBeauget <azentiaytbpro@gmail.com>
2024-06-22 16:06:34 +0200

    feat: finish stripe integration

--- android/app/src/main/kotlin/com/twins/twins_front/MainActivity.kt
+++ android/app/src/main/kotlin/com/twins/twins_front/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.twins.twins_front
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/services/payment_service.dart
+++ lib/services/payment_service.dart
@@ -4,28 +4,32 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 Future createPaymentIntent(
-    {required String name,
-    required String adresss,
-    required String pin,
-    required String city,
-    required String state,
-    required String country,
-    required String currency,
-    required String amount}) async {
+    {required String currency,
+    required String amount,
+    required String? firstName,
+    required String? email}) async {
   final paymentUrl = Uri.parse(dotenv.env["PAYMENT_URL"]!);
   final String? secretKey = dotenv.env["STRIPE_SECRET_KEY"];
+  final body = {
+    'amount': amount,
+    'currency': currency.toLowerCase(),
+    'automatic_payment_methods[enabled]': 'true',
+    'receipt_email': email,
+    'metadata[name]': firstName,
+    'description': 'Twins subscription'
+  };
 
-  final response = await http.post(paymentUrl,
-      headers: {
-        "Authorization": "Bearer $secretKey",
-        "Content-type": 'application/x-www-form-urlencoded'
-      },
-      body: "any");
+  try {
+    final response = await http.post(paymentUrl,
+        headers: {
+          "Authorization": "Bearer $secretKey",
+          "Content-type": 'application/x-www-form-urlencoded'
+        },
+        body: body);
 
-  if (response.statusCode == 200) {
     var json = jsonDecode(response.body);
-    print(json);
-  } else {
-    print("nul");
+    return json;
+  } catch (e) {
+    rethrow;
   }
 }
